55636db0b8f86b4950f305d428cb9f3841a725a8
Mulyukov Ruslan <ret8558@mail.ru>
2013-11-10 13:02:02 +0400

    clean

--- app/models/thing.rb
+++
@@ -1,7 +0,0 @@
-class Thing < ActiveRecord::Base
-  attr_accessible :content, :image, :manufacturer_id, :title, :remote_image_url
-
-  belongs_to :manufacturer
-
-  mount_uploader :image, ImageUploader
-end
